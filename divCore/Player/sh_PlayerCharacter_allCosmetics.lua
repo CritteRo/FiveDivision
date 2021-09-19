055de6b8976fd6852cf65bceedcb5a1decaca28e
CritteRo <critteras@gmail.com>
2021-09-19 12:03:29 +0300

    Added all hairstyle cosmetics + all badges

--- divCore/Player/sh_PlayerCharacter_allCosmetics.lua
+++ divCore/Player/sh_PlayerCharacter_allCosmetics.lua
@@ -38,6 +38,437 @@ cosmeticClothes = {
             {"comp2", 10, "Default Hairstyle"},
         },
     },
+---------HAIR STYLES--------------------------------------------------------------------
+    [100] = {
+        ['mp_m_freemode_01'] = {
+            {'comp2', 0, "Bald / No hairstyle"},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp2', 0, "Bald / No hairstyle"},
+        },
+    },
+    [101] = {
+        ['mp_m_freemode_01'] = {
+            {'comp2', 1, "Buzz Cut Hairstyle"},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp2', 1, "Buzz Cut Hairstyle"},
+        },
+    },
+    [102] = {
+        ['mp_m_freemode_01'] = {
+            {'comp2', 3, "'Fresh Cut' Hairstyle"},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp2', 3, "'Fresh Cut' Hairstyle"},
+        },
+    },
+    [103] = {
+        ['mp_m_freemode_01'] = {
+            {'comp2', 4, "'Middle Management' Hairstyle"},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp2', 4, "'Middle Management' Hairstyle"},
+        },
+    },
+    [104] = {
+        ['mp_m_freemode_01'] = {
+            {'comp2', 5, "'Upper Management' Hairstyle"},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp2', 5, "'Upper Management' Hairstyle"},
+        },
+    },
+    [105] = {
+        ['mp_m_freemode_01'] = {
+            {'comp2', 6, "'Punk 1' Hairstyle"},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp2', 6, "'Punk 1' Hairstyle"},
+        },
+    },
+    [106] = {
+        ['mp_m_freemode_01'] = {
+            {'comp2', 7, "'Suburb 1' Hairstyle"},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp2', 7, "'Suburb 1' Hairstyle"},
+        },
+    },
+    [107] = {
+        ['mp_m_freemode_01'] = {
+            {'comp2', 8, "Rasta Hairstyle"},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp2', 8, "Rasta Hairstyle"},
+        },
+    },
+    [108] = {
+        ['mp_m_freemode_01'] = {
+            {'comp2', 9, "'Vespucci Hobo' Hairstyle"},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp2', 9, "'Vespucci Hobo' Hairstyle"},
+        },
+    },
+    [109] = {
+        ['mp_m_freemode_01'] = {
+            {'comp2', 10, "Hair10 No hairstyle"},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp2', 2, "'Karen' hairstyle"},
+        },
+    },
+    [110] = {
+        ['mp_m_freemode_01'] = {
+            {'comp2', 11, "11 Hairstyle"},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp2', 11, "11 Hairstyle"},
+        },
+    },
+    [111] = {
+        ['mp_m_freemode_01'] = {
+            {'comp2', 12, "12 Hairstyle"},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp2', 12, "12 Hairstyle"},
+        },
+    },
+    [112] = {
+        ['mp_m_freemode_01'] = {
+            {'comp2', 13, "13 Hairstyle"},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp2', 13, "13 Hairstyle"},
+        },
+    },
+    [113] = {
+        ['mp_m_freemode_01'] = {
+            {'comp2', 14, "14 Hairstyle"},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp2', 14, "14 Hairstyle"},
+        },
+    },
+    [114] = {
+        ['mp_m_freemode_01'] = {
+            {'comp2', 15, "15 Hairstyle"},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp2', 15, "15 Hairstyle"},
+        },
+    },
+    [115] = {
+        ['mp_m_freemode_01'] = {
+            {'comp2', 16, "16 Hairstyle"},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp2', 16, "16 Hairstyle"},
+        },
+    },
+    [116] = {
+        ['mp_m_freemode_01'] = {
+            {'comp2', 17, "17 Hairstyle"},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp2', 17, "17 Hairstyle"},
+        },
+    },
+    [117] = {
+        ['mp_m_freemode_01'] = {
+            {'comp2', 18, "18 Hairstyle"},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp2', 18, "18 Hairstyle"},
+        },
+    },
+    [118] = {
+        ['mp_m_freemode_01'] = {
+            {'comp2', 19, "19 Hairstyle"},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp2', 19, "19 Hairstyle"},
+        },
+    },
+    [119] = {
+        ['mp_m_freemode_01'] = {
+            {'comp2', 20, "20 Hairstyle"},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp2', 20, "20 Hairstyle"},
+        },
+    },
+    [120] = {
+        ['mp_m_freemode_01'] = {
+            {'comp2', 21, "21 Hairstyle"},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp2', 21, "21 Hairstyle"},
+        },
+    },
+    [121] = {
+        ['mp_m_freemode_01'] = {
+            {'comp2', 22, "22 Hairstyle"},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp2', 22, "22 Hairstyle"},
+        },
+    },
+    [122] = {
+        ['mp_m_freemode_01'] = {
+            {'comp2', 24, "24 Hairstyle"},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp2', 23, "23 Hairstyle"},
+        },
+    },
+    [123] = {
+        ['mp_m_freemode_01'] = {
+            {'comp2', 25, "25 Hairstyle"},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp2', 25, "25 Hairstyle"},
+        },
+    },
+    [124] = {
+        ['mp_m_freemode_01'] = {
+            {'comp2', 26, "26 Hairstyle"},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp2', 26, "26 Hairstyle"},
+        },
+    },
+    [125] = {
+        ['mp_m_freemode_01'] = {
+            {'comp2', 27, "27 Hairstyle"},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp2', 27, "27 Hairstyle"},
+        },
+    },
+    [126] = {
+        ['mp_m_freemode_01'] = {
+            {'comp2', 28, "28 Hairstyle"},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp2', 28, "28 Hairstyle"},
+        },
+    },
+    [127] = {
+        ['mp_m_freemode_01'] = {
+            {'comp2', 29, "29 Hairstyle"},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp2', 29, "29 Hairstyle"},
+        },
+    },
+    [128] = {
+        ['mp_m_freemode_01'] = {
+            {'comp2', 30, "30 Hairstyle"},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp2', 30, "30 Hairstyle"},
+        },
+    },
+    [129] = {
+        ['mp_m_freemode_01'] = {
+            {'comp2', 31, "31 Hairstyle"},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp2', 31, "31 Hairstyle"},
+        },
+    },
+    [130] = {
+        ['mp_m_freemode_01'] = {
+            {'comp2', 32, "32 Hairstyle"},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp2', 32, "32 Hairstyle"},
+        },
+    },
+    [131] = {
+        ['mp_m_freemode_01'] = {
+            {'comp2', 33, "33 Hairstyle"},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp2', 33, "33 Hairstyle"},
+        },
+    },
+    [132] = {
+        ['mp_m_freemode_01'] = {
+            {'comp2', 34, "34 Hairstyle"},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp2', 34, "34 Hairstyle"},
+        },
+    },
+    [133] = {
+        ['mp_m_freemode_01'] = {
+            {'comp2', 35, "35 Hairstyle"},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp2', 35, "35 Hairstyle"},
+        },
+    },
+    [134] = {
+        ['mp_m_freemode_01'] = {
+            {'comp2', 72, "72 Hairstyle"},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp2', 36, "36 Hairstyle"},
+        },
+    },
+    [135] = {
+        ['mp_m_freemode_01'] = {
+            {'comp2', 73, "73 Hairstyle"},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp2', 37, "37 Hairstyle"},
+        },
+    },
+    [136] = {
+        ['mp_m_freemode_01'] = {
+            {'comp2', 74, "74 Hairstyle"},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp2', 38, "38 Hairstyle"},
+        },
+    },
+    [137] = {
+        ['mp_m_freemode_01'] = {
+            {'comp2', 75, "75 Hairstyle"},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp2', 76, "76 Hairstyle"},
+        },
+    },
+    [138] = {
+        ['mp_m_freemode_01'] = {
+            {'comp2', 70, "77 Hairstyle"},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp2', 77, "77 Hairstyle"},
+        },
+    },
+    [139] = {
+        ['mp_m_freemode_01'] = {
+            {'comp2', 68, "78 Hairstyle"},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp2', 78, "78 Hairstyle"},
+        },
+    },
+    [140] = {
+        ['mp_m_freemode_01'] = {
+            {'comp2', 66, "79 Hairstyle"},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp2', 79, "79 Hairstyle"},
+        },
+    },
+---------------------Badges, ties and military pouch-----------------------
+    [150] = {
+        ['mp_m_freemode_01'] = {
+            {'comp7', 125, "IAA badge and Police badge"},
+            {'comp9', 0},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp7', 95, "IAA badge and Police badge"},
+            {'comp9', 0},
+        },
+    },
+    [151] = {
+        ['mp_m_freemode_01'] = {
+            {'comp7', 128, "IAA badge"},
+            {'comp9', 0},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp7', 98, "IAA badge"},
+            {'comp9', 0},
+        },
+    },
+    [152] = {
+        ['mp_m_freemode_01'] = {
+            {'comp7', 127, "Paramedic badge"},
+            {'comp9', 0},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp7', 97, "Paramedic badge"},
+            {'comp9', 0},
+        },
+    },
+    [153] = {
+        ['mp_m_freemode_01'] = {
+            {'comp7', 126, "Paramedic thingy"},
+            {'comp9', 0},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp7', 96, "Paramedic thingy"},
+            {'comp9', 0},
+        },
+    },
+    [154] = {
+        ['mp_m_freemode_01'] = {
+            {'comp7', 123, "Lucky Charm"},
+            {'comp9', 0},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp7', 93, "Lucky Charm"},
+            {'comp9', 0},
+        },
+    },
+    [155] = {
+        ['mp_m_freemode_01'] = {
+            {'comp7', 112, "Scarf"},
+            {'comp9', 0},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp7', 15, "Scarf"},
+            {'comp9', 0},
+        },
+    },
+    [156] = {
+        ['mp_m_freemode_01'] = {
+            {'comp7', 37, "Unorganized Tie"},
+            {'comp9', 0},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp7', 20, "Unorganized Tie"},
+            {'comp9', 0},
+        },
+    },
+    [157] = {
+        ['mp_m_freemode_01'] = {
+            {'comp7', 38, "Organized Tie"},
+            {'comp9', 0},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp7', 22, "Organized Tie"},
+            {'comp9', 0},
+        },
+    },
+    [158] = {
+        ['mp_m_freemode_01'] = {
+            {'comp9', 54, "FIB Badge"},
+            {'comp7', 0},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp9', 54, "FIB Badge"},
+            {'comp7', 0},
+        },
+    },
+    [159] = {
+        ['mp_m_freemode_01'] = {
+            {'comp7', 147, "Military Pouches"},
+            {'comp9', 0},
+        },
+        ['mp_f_freemode_01'] = {
+            {'comp7', 116, "Military Pouches"},
+            {'comp9', 0},
+        },
+    },
+-----------------------------------------------
     [5] = {
         ['mp_m_freemode_01'] = {
             {"comp3", 1, "Sports Jacket"},
@@ -64,78 +495,6 @@ cosmeticClothes = {
         },
         ['mp_f_freemode_01'] = {
             {"comp4", 1, "Regular Jeans"},
-        },
-    },
-    [8] = {
-        ['mp_m_freemode_01'] = {
-            {'comp2', 0, "Bald / No hairstyle"},
-        },
-        ['mp_f_freemode_01'] = {
-            {'comp2', 0, "Bald / No hairstyle"},
-        },
-    },
-    [9] = {
-        ['mp_m_freemode_01'] = {
-            {'comp2', 1, "Buzz Cut Hairstyle"},
-        },
-        ['mp_f_freemode_01'] = {
-            {'comp2', 1, "Buzz Cut Hairstyle"},
-        },
-    },
-    [10] = {
-        ['mp_m_freemode_01'] = {
-            {'comp2', 3, "'Fresh Cut' Hairstyle"},
-        },
-        ['mp_f_freemode_01'] = {
-            {'comp2', 3, "'Fresh Cut' Hairstyle"},
-        },
-    },
-    [11] = {
-        ['mp_m_freemode_01'] = {
-            {'comp2', 4, "'Middle Management' Hairstyle"},
-        },
-        ['mp_f_freemode_01'] = {
-            {'comp2', 4, "'Middle Management' Hairstyle"},
-        },
-    },
-    [12] = {
-        ['mp_m_freemode_01'] = {
-            {'comp2', 5, "'Upper Management' Hairstyle"},
-        },
-        ['mp_f_freemode_01'] = {
-            {'comp2', 5, "'Upper Management' Hairstyle"},
-        },
-    },
-    [13] = {
-        ['mp_m_freemode_01'] = {
-            {'comp2', 6, "'Punk 1' Hairstyle"},
-        },
-        ['mp_f_freemode_01'] = {
-            {'comp2', 6, "'Punk 1' Hairstyle"},
-        },
-    },
-    [14] = {
-        ['mp_m_freemode_01'] = {
-            {'comp2', 7, "'Suburb 1' Hairstyle"},
-        },
-        ['mp_f_freemode_01'] = {
-            {'comp2', 7, "'Suburb 1' Hairstyle"},
-        },
-    },
-    [15] = {
-        ['mp_m_freemode_01'] = {
-            {'comp2', 8, "Rasta Hairstyle"},
-        },
-        ['mp_f_freemode_01'] = {
-            {'comp2', 8, "Rasta Hairstyle"},
-        },
-    },
-    [16] = {
-        ['mp_m_freemode_01'] = {
-            {'comp2', 9, "'Vespucci Hobo' Hairstyle"},
-        },
-        ['mp_f_freemode_01'] = {
-            {'comp2', 9, "'Vespucci Hobo' Hairstyle"},
         },
     },
 }
