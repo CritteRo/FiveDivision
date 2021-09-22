c79fc536569f817e5f69d0d661ca81d8835727a4
CritteRo <critteras@gmail.com>
2021-09-22 12:55:07 +0300

    new outpost

--- divOutposts/shared/sh_outpost_main.lua
+++ divOutposts/shared/sh_outpost_main.lua
@@ -1,7 +1,8 @@
 outposts = {
-    [1] = {name = "Alta St Contruction Site", status = 2--[[0 = enemy, 1 = abandoned, 2 = liberated]],blipX = 34.67, blipY = -410.101, blipZ = 45.601, x1 = 39.68, y1 = -371.78, z1 = 63.801, x2 = 26.08, y2 = -448.27, z2 = 74.98, x3 = -27.95, y3 = -445.02, z3 = 80.992, rx = 0.0, ry = 0.0, rz = -68.821, xp = 0, cash = 0},
+    [1] = {name = "Alta St Contruction Site", status = 0--[[0 = enemy, 1 = abandoned, 2 = liberated]],blipX = 34.67, blipY = -410.101, blipZ = 45.601, x1 = 39.68, y1 = -371.78, z1 = 63.801, x2 = 26.08, y2 = -448.27, z2 = 74.98, x3 = -27.95, y3 = -445.02, z3 = 80.992, rx = 0.0, ry = 0.0, rz = -68.821, xp = 0, cash = 0},
     [2] = {name = "Premium Deluxe Motorsport", status = 0--[[0 = enemy, 1 = abandoned, 2 = liberated]],blipX = -51.86, blipY = -1112.39, blipZ = 26.44, x1 = -30.25, y1 = -1104.98, z1 = 25.42, x2 = -44.41, y2 = -1095.54, z2 = 35.16, x3 = -91.08, y3 = -1127.41, z3 = 37.101, rx = 0.0, ry = 0.0, rz = -62.34, xp = 0, cash = 0},
-    [3] = {name = "Little Seul Construction Site", status = 1--[[0 = enemy, 1 = abandoned, 2 = liberated]],blipX = -474.01, blipY = -915.26, blipZ = 29.41, x1 = -448.403, y1 = -940.547, z1 = 37.503, x2 = -501.62, y2 = -989.148, z2 = 53.776, x3 = -501.166, y3 = -886.558, z3 = 51.06, rx = 0.0, ry = 0.0, rz = -163.945, xp = 0, cash = 0},
+    [3] = {name = "Little Seul Construction Site", status = 0--[[0 = enemy, 1 = abandoned, 2 = liberated]],blipX = -474.01, blipY = -915.26, blipZ = 29.41, x1 = -448.403, y1 = -940.547, z1 = 37.503, x2 = -501.62, y2 = -989.148, z2 = 53.776, x3 = -501.166, y3 = -886.558, z3 = 51.06, rx = 0.0, ry = 0.0, rz = -163.945, xp = 0, cash = 0},
+    [4] = {name = "Pillbox Rooftop Rumble", status = 0--[[0 = enemy, 1 = abandoned, 2 = liberated]],blipX = 127.383, blipY = -1087.175, blipZ = 46.347, x1 = 140.14, y1 = -1090.71, z1 = 49.14, x2 = 174.46, y2 = 1095.78, z2 = 52.705, x3 = 112.613, y3 = -1069.68, z3 = 47.36, rx = 0.0, ry = 0.0, rz = -123.282, xp = 0, cash = 0},
 }
 
 enemySpawns = {
@@ -21,6 +22,17 @@ enemySpawns = {
         {handle = 0, x = -53.84, y = -1090.71, z = 26.42},
         {handle = 0, x = -62.85, y = -1102.47, z = 26.26},
         {handle = 0, x = -46.09, y = -1115.46, z = 26.44},
+    },
+    [4] = {
+        {handle = 0, x = 125.77, y = -1088.19, z = 30.601, h = 34.193},
+        {handle = 0, x = 121.53, y = -1088.31, z = 30.569, h = 332.89},
+        {handle = 0, x = 120.13, y = -1106.27, z = 37.473, h = 251.51},
+        {handle = 0, x = 125.71, y = -1106.55, z = 38.065, h = 85.066},
+        {handle = 0, x = 127.81, y = -1087.62, z = 45.556, h = 43.653},
+        {handle = 0, x = 133.14, y = -1111.04, z = 45.566, h = 270.87},
+        {handle = 0, x = 144.11, y = -1095.58, z = 52.387, h = 96.524},
+        {handle = 0, x = 162.25, y = -1105.26, z = 49.155, h = 11.840},
+        {handle = 0, x = 176.82, y = -1095.51, z = 52.291, h = 274.86},
     },
 }
 
