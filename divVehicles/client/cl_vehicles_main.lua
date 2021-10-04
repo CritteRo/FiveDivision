fef8a439575fe19d9035e82c77bd202a17668aa4
CritteRo <critteras@gmail.com>
2021-10-04 11:36:26 +0300

    test: moving wait from start of loop to end of it...maybe this will fix the nil error in divVehicles

--- divVehicles/client/cl_vehicles_main.lua
+++ divVehicles/client/cl_vehicles_main.lua
@@ -2,11 +2,11 @@ myRoadCoords = {}
 
 Citizen.CreateThread(function()
     while true do
-        Citizen.Wait(3000)
         local coords = GetEntityCoords(PlayerPedId()) 
         local ret, _roadCoords, _heading = GetClosestVehicleNodeWithHeading(coords.x + math.random(-50, 50), coords.y + math.random(-50, 50), coords.z, 1, 3.0, 0)
         local retval, _sideCoords = GetPointOnRoadSide(_roadCoords.x, _roadCoords.y, _roadCoords.z)
         myRoadCoords = {roadCoords = _roadCoords, sideCoords = _sideCoords, heading = _heading}
         TriggerServerEvent('vehicles.UpdateMyCoords', myRoadCoords)
+        Citizen.Wait(3000)
     end
 end)
